69033a4a1833804e90be92aaa029c55b2738fb5d
Ron Cordell <ron.cordell@berkeley.edu>
2015-07-05 10:29:12 -0700

    modified mongo serializer to not use auth and other corrections

--- search/mongoTweetSerializer.py
+++ search/mongoTweetSerializer.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import json
-import StringIO
 
 # tweet serializer class from the activities document
 class MongoTweetSerializer():
@@ -19,6 +18,5 @@ class MongoTweetSerializer():
 
 
 	def initPersistance(self, database, collection):
-		password = 'password'
-		self._client = MongoClient('mongodb://testuser:' + password + '@192.168.194.167',port=27017)
-		self._collection = self._client['database']['collection']
+		self._client = MongoClient('127.0.0.1', port=27017)
+		self._collection = self._client[str(database)][str(collection)]
