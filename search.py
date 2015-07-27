4a0cd53a81e388dc2725646dfbe275bd1af105a1
Ron Cordell <ron.cordell@berkeley.edu>
2015-07-26 22:29:31 -0700

    Fix up the search.py with argparser and rearrangement of classes into more sane structure

--- search.py
+++ search.py
@@ -2,7 +2,65 @@ import sys
 import datetime
 import urllib
 from twitterutils import tweetFetcher
+from serializers.mongoTweetSerializer import MongoTweetSerializer
+from serializers.s3ChunkedTweetSerializer import S3ChunkedTweetSerializer
+from serializers.consoleTweetSerializer import ConsoleTweetSerializer
 from twitterutils import apikeys
+import argparse  
+
+parser = argparse.ArgumentParser(description = 'Search the Twitter API')
+parser.add_argument('-db','--database',
+					type=str,
+					required=False,
+					default=None,
+					help='MongoDB database to store tweets')
+parser.add_argument('-c', '--collection',
+					type=str,
+					required=False,
+					default=None,
+					help='MongoDB collection to store tweets')
+parser.add_argument('-s', '--server',
+					type=str,
+					required=False,
+					default='127.0.0.1',
+					help='MongoDB server to serialize data')
+parser.add_argument('-t', '--track',
+					type=str,
+					required=True,
+					help='Twitter phrases to search')
+parser.add_argument('-s3','--s3url',
+					type=str,
+					required=False,
+					default=None,
+					help='Amazon S3 storage to store tweets')
+parser.add_argument('-si','--since',
+					type=str,
+					required=False,
+					default=None,
+					help='lower bound search date YYYY-MM-DD')
+parser.add_argument('-u','--until',
+					type=str,
+					required=False,
+					default=None,
+					help='upper bound search date YYYY-MM-DD')
+
+args = parser.parse_args()
+
+fetchSize = 1500
+
+# pick the serializer
+if args.database:
+	if (args.collection):
+		serializer = MongoTweetSerializer(args.server, args.database , args.collection)
+	else:
+		print "MongoDB database and collection must be specified when using MongoDB server"
+		sys.exit(1)
+elif args.s3url:
+	serializer = S3ChunkedTweetSerializer(args.s3url)
+else:
+	print "Writing tweets to console..."
+	serializer = ConsoleTweetSerializer()
+	fetchSize = 10
 
 # time and date formats
 xsdDatetimeFormat = "%Y-%m-%dT%H:%M:%S"
@@ -23,23 +81,17 @@ def date_partition(start,end):
    
 #   for d in date_partition(start,end):
 	
-if (len(sys.argv) > 1):
-	q = urllib.quote(sys.argv[1])
-else:
-	print "missing query argument, exiting...."
-	sys.exit()
+q = urllib.quote(args.track)
 
-if (len(sys.argv) > 2):
-	if (sys.argv[2] != None):
-		start = datetime.datetime.strptime(sys.argv[2],xsdDateFormat) # start date
-		q = q + " since:" + start.strftime(xsdDateFormat)
+if (args.since):
+	start = datetime.datetime.strptime(args.since,xsdDateFormat) # start date
+	q = q + " since:" + start.strftime(xsdDateFormat)
 
-if (len(sys.argv) > 3):
-	if (sys.argv[3] != None):
-		end = datetime.datetime.strptime(sys.argv[3],xsdDateFormat)   # end date
+if(args.until):
+		end = datetime.datetime.strptime(args.until,xsdDateFormat)   # end date
 		q = q + " until:" + end.strftime(xsdDateFormat)
 
-print q
+print "Searching for: " + q
 
-fetcher = tweetFetcher.TweetFetcher(apikeys.NONE)
+fetcher = tweetFetcher.TweetFetcher(serializer, apikeys.W205_ASSIGNEMNT3, fetchSize=fetchSize)
 fetcher.search(q)
--- twitterutils/tweetFetcher.py
+++ twitterutils/tweetFetcher.py
@@ -3,18 +3,14 @@ import threading
 import tweepy
 import twitterAuth
 
-#from s3ChunkedTweetSerializer import S3ChunkedTweetSerializer
-from serializers.mongoTweetSerializer import MongoTweetSerializer
-
 class TweetFetcher:
 
-	def __init__(self, twitterKey):
+	def __init__(self, serializer, twitterKey, fetchSize=1500):
+		self._serializer = serializer
+		self._fetchSize = fetchSize
 		auth = twitterAuth.getAppAuth(twitterKey)
             
 		self.api = tweepy.API(auth_handler=auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
-
-#		self.serializer = S3ChunkedTweetSerializer(200, 'W205-Assignment2-RC-test')
-		self.serializer = MongoTweetSerializer('192.168.194.171','twitter_db','tweets')
 
 		signal.signal(signal.SIGINT, self.interrupt)
 
@@ -22,13 +18,12 @@ class TweetFetcher:
 		self._lock = threading.RLock()
 
 	def interrupt(self, signum, frame):
-		print("CTRL-C caught, closing...")
+		print(" CTRL-C caught, closing...")
 		with self._lock:
-			self.serializer.end()
+			self._serializer.end()
 		exit(1)
 
 	def search(self, q):
-		for tweet in tweepy.Cursor(self.api.search,q=q, count=1500).items():
-			with self._lock:
-				self.serializer.write(tweet._json)
-		self.serializer.end()
+		for tweet in tweepy.Cursor(self.api.search,q=q, count=self._fetchSize).items():
+			self._serializer.write(tweet._json)
+		self._serializer.end()
